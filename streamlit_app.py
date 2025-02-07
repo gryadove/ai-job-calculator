a876582cdf0b5f10d5b0d5004fbb7c894a4087f6
Your GitHub gryadove <gryadovkina@gmail.com>
2025-02-07 14:27:38 -0500

    spacing

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,10 @@ with col1:
         key="high_initial",
         help="Enter your assumption regarding % of total 1st Tier Content/Admin jobs that will be lost due to AI in 2025"
     ) 
-
+    # Just use empty lines for spacing
+    st.write("")
+    st.write("")
+    
     st.write("% Change in the Job Loss Rate Post 2025: Tier-1 Jobs")
     high_change = st.number_input(
         "",
